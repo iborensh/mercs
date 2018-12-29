af9958f8796ef05920f79b4311a73585f1c03632
Shahar Rosentraub <srosentr@cisco.com>
2018-12-29 22:45:05 +0200

    map2goals v0

--- server/add_functions.py
+++ server/add_functions.py
@@ -30,3 +30,31 @@ json_input = {
 "modified_on" : ISODate("2018-12-27T09:51:13.725Z")
 }
 
+
+def map2goals(map_json):
+    class Path:
+        def __init__(self, m_type, field, outcome, attributes):
+            self.m_type = m_type
+            self.field = field
+            self.outcome = outcome
+            self.attributes = attributes
+
+        def print_path(self):
+            print
+            "produce a ", self.field, self.outcome, "using ", self.attributes
+
+    paths_list = []
+    for m_type in json_input['content']['type']:
+        for field in json_input['content']['field']:
+            for outcome in json_input['content']['outcome']:
+                for attributes in json_input['content']['attributes']:
+                    p = Path(m_type, field, outcome, attributes)
+                    paths_list.append(p)
+
+    return paths_list
+
+def goals2skills(goals_file):
+    return "skills"
+
+def skills2reward(sills_file):
+    return "reward"
