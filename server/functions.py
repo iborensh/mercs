67db96537266be957e3367e87f3d4be87945758f
Shahar Rosentraub <srosentr@cisco.com>
2019-02-03 22:32:58 +0200

    1. added flattening to the json structures so removed all the irelevant files 2. started building the find_bands_for_mission function 3. added code to transfer mission requirement dict to df which can be used to find best bands

--- server/functions.py
+++ server/functions.py
@@ -1,8 +1,22 @@
 import json, os
 import pandas as pd
+from pandas.io.json import json_normalize
 
-def find_similarity(a,b):
-    print a
+def find_bands_for_mission(mission_requirements):
+
+    #generate a mission requirements df for joining with the bands df
+    mission_requirements_df = json_normalize(mission_requirements).stack().reset_index().drop('level_0', axis=1)
+    mission_requirements_df.columns = ['mission_skills', 'mission_skills_value']
+    print mission_requirements_df
+
+    # open the skills json file
+    with open('{}/jsons/skill_tree.json'.format(os.getcwd())) as f:
+        d = json.load(f)
+
+    # transfer the skills json file to a flat df
+    skills_df = json_normalize(d, record_path=["options",'options'], meta=['label', 'value'], meta_prefix='meta_')
+
+    print skills_df.head()
 
 
 
@@ -12,14 +26,5 @@ def find_similarity(a,b):
 
 
 
-print os.getcwd()
-
-with open('{}/jsons/online_courses.json'.format(os.getcwd())) as s:
-    courses = json.load(s)
-
-# with open('{}/jsons/json_test.json'.format(os.getcwd())) as p:
-#     print p.read()
-skills = pd.read_json('/Users/srosentr 1/mercs_vcs/server/jsons/skill_tree.json')
-print skills.head()
-
-find_similarity(skills, "yo")
+mission_requirements = {"sql": 5, "sw_general": 3}
+find_bands_for_mission(mission_requirements)
